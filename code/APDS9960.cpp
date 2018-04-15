e07cab7b2da87d1b961f4c25cc7afc8402566b9a
jingwang9 <35493495+jingwang9@users.noreply.github.com>
2018-04-15 13:16:42 +0300

    Update APDS9960.cpp

--- code/APDS9960.cpp
+++ code/APDS9960.cpp
@@ -1,22 +1,22 @@
 #include "APDS9960.h"
-
+//Apply an APDS9960_RPi object when constructing
 APDS9960::APDS9960()
 {
 	apds = new APDS9960_RPi();
 }
-
+//Delete the APDS9960_RPi object when it is destroyed
 APDS9960::~APDS9960()
 {
 	delete apds;
 }
-
+//Initialize hardware and call the related interface
 void APDS9960::init(void)
 {
 	apds->init();
 	apds->setProximityGain(PGAIN_2X);
 	apds->enableProximitySensor(false);
 }
-
+//Call related interface to get the distance
 unsigned char APDS9960::read(void)
 {
 	uint8_t proximity_data = 0;
